69a81869988ae7a148b1b3a97486fa9ca989ac0a
Saurabh Dange <saurabhdange94@gmail.com>
2025-09-07 22:00:43 +0530

    Updated project: fixed bugs, updated model, updated templates

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask , jsonify, request
+from flask import Flask , jsonify, request,render_template
 import util
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Flask server is running! Use /get_location_names or /predict_home_price"
-
+     return render_template("app.html")
 
 @app.route('/get_location_names' , methods=['GET'])
 def get_location_names():
